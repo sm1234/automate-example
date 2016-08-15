dd00ccd08bf1992a5686ba81068013425a79d023
sm1234 <saurabhmehta123@gmail.com>
2016-08-15 11:22:40 +0530

    Enabled multiple browser settings

--- test.java
+++ test.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import com.esotericsoftware.yamlbeans.YamlReader;
 
 import java.net.URL;
 
@@ -15,21 +16,32 @@ public class JavaSample {
 
   public static void main(String[] args) throws Exception {
 
-    DesiredCapabilities caps = new DesiredCapabilities();
-    caps.setCapability("browser", "IE");
-    caps.setCapability("browser_version", "7.0");
-    caps.setCapability("os", "Windows");
-    caps.setCapability("os_version", "XP");
-    caps.setCapability("browserstack.debug", "true");
 
-    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-    driver.get("http://www.google.com");
-    WebElement element = driver.findElement(By.name("q"));
 
-    element.sendKeys("BrowserStack");
-    element.submit();
+    YamlReader reader = new YamlReader(new FileReader(".automate.yml"));
+    Object object = reader.read();
+    Map config = (Map)object;
+    List listOfBrowserSettings = (List)config.get("desired capabilities");
+    
+    for (int BrowserSettingIndex = 0; BrowserSettingIndex < listOfBrowserSettings.length; BrowserSettingIndex++) 
+    {
+      DesiredCapabilities caps = new DesiredCapabilities();
+      caps.setCapability("browser", listOfBrowserSettings[BrowserSettingIndex].get("browser"));
+      caps.setCapability("browser_version", listOfBrowserSettings[BrowserSettingIndex].get("browser_version"));
+      caps.setCapability("os", listOfBrowserSettings[BrowserSettingIndex].get("os"));
+      caps.setCapability("os_version", listOfBrowserSettings[BrowserSettingIndex].get("os_version"));
+      caps.setCapability("browserstack.debug", listOfBrowserSettings[BrowserSettingIndex].get("browserstack.debug"));
+  
+      WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+      driver.get("http://www.google.com");
+      WebElement element = driver.findElement(By.name("q"));
+  
+      element.sendKeys("BrowserStack");
+      element.submit();
+  
+      System.out.println(driver.getTitle());      
+    }
 
-    System.out.println(driver.getTitle());
     driver.quit();
 
   }
